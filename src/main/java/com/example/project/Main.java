3cb2af8109e471de1c0a6e3053b1ef524bcd5efa
mhayescs19 <mhayescs19@gmail.com>
2021-01-28 09:12:08 -0800

    runtime assets

--- src/main/java/com/example/project/Main.java
+++ src/main/java/com/example/project/Main.java
@@ -15,6 +15,15 @@ public class Main {
         SpringApplication.run(Main.class, args);
     }
 
+
+
+
+
+
+
+
+
+
     @GetMapping("/hello")
     public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
