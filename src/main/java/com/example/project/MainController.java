7d3517265b128e52957ca3f295fcaa46d6d57fb5
mhayescs19 <mhayescs19@gmail.com>
2021-01-29 20:45:05 -0800

    fix to re add Java side in MainController.java for michael.html

--- src/main/java/com/example/project/MainController.java
+++ src/main/java/com/example/project/MainController.java
@@ -60,7 +60,15 @@ public class MainController {
 
 
     @GetMapping("/ap-practice/michael") // root for Michael page
-    public String apPracticeMichael(){
-        return "ap-practice/michael.html";
+    public String apPracticeMichael(@RequestParam (name="length", required=false, defaultValue="4") String length, @RequestParam(name="prefix", required=false, defaultValue="A") String prefix, Model model){ // use multiple @RequestParam annotations if you have multiple variables in HTML (correct way to reference them?)
+
+        // UNIT 5 FRQ PART A
+        PasswordGenerator newPassword = new PasswordGenerator(Integer.parseInt(length), prefix);
+
+
+        model.addAttribute("passwords", newPassword.getPasswordList()); // s: goes to variable name in HTML
+        model.addAttribute("passwordGenerated", newPassword.pwGen());
+
+        return "ap-practice/michael"; // use path to HTML file without .html OR it does not recognize model attributes!
     }
 }
