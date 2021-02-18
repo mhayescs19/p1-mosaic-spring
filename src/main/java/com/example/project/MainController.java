057a9fbbd47cb7674c20bc4995c175dc93ace681
Nakul Nandhakumar <nakul@nandhakumar.net>
2021-02-17 21:55:04 -0800

    Front End kinda working for teacher view

--- src/main/java/com/example/project/MainController.java
+++ src/main/java/com/example/project/MainController.java
@@ -5,12 +5,17 @@ import com.example.project.ap.practice.Invitation;
 import com.example.project.mini.games.Hangman;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import AndrewFrq1.StringStruct;
 
 import com.example.project.ap.practice.PasswordGenerator;
 import com.example.project.ap.practice.Consecutive;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import synergy.Synergy;
+
+import javax.validation.Valid;
 
 @Controller
 public class MainController {
@@ -134,8 +139,25 @@ public class MainController {
         return "labs/hangman";
     }
 
-    @GetMapping("/synergy") // root for synergy page
+
+
+    @GetMapping("/synergy")
     public String Synergy() {
         return "synergy/loginView";
+    }
+
+    @GetMapping("/teacherView")
+    public String showForm(Synergy synergy) {
+        return "synergy/teacherView";
+    }
+
+    @PostMapping("/teacherView")
+    public String teacherView(@Valid Synergy synergy, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "synergy/teacherView";
+        }
+        model.addAttribute("student", synergy.getName()+synergy.getAge()+synergy.getGrade()
+        +synergy.getYear());
+        return "synergy/teacherView";
     }
 }
--- src/main/java/synergy/Synergy.java
+++ src/main/java/synergy/Synergy.java
@@ -2,42 +2,42 @@ package synergy;
 
 import lombok.Getter;
 import lombok.Setter;
+
+import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.util.Date;
-import org.springframework.format.annotation.DateTimeFormat;
 
 
 @Setter
 @Getter
 public class Synergy {
-    /* Used to build JSON */
-    static public String nameKy = "Name";
-    static public String ageKy = "Age";
-    static public String dobKy = "DOB";
-    static public String adoptKy = "Adopted";
+    public Synergy synergy;
 
-    /*
-    @NotNull: Does not allow a null value, which is what Spring MVC generates if the entry is empty.
-    @Size(min=2, max=30): Allows names between 2 and 30 characters long.
-     */
+    static public String nameKy = "Name";
+    static public String yearKy = "Year";
+    static public String gradeKy = "Grade";
+    static public String ageKy = "Age";
+
+
     @NotNull
-    @Size(min = 2, max = 30)
+    @Size(min = 2, max = 50)
     private String name;
 
-    // @Min(18): Does not allow the age to be less than 18.
     @NotNull
-    @Min(16)
+    @Min(1)
+    @Max(99)
     private Integer age;
 
-    @DateTimeFormat(pattern = "MM/dd/yyyy")
-    private Date dob;
+    @NotNull
+    private String year;
 
-    public Person() {}
+    @NotNull
+    @Size(min = 1, max = 2)
+    private String grade;
 
-    public Person(String name, Integer age) {this.name = name; this.age = age;}
+    public Synergy() {
 
-    public Person(String name, Integer age, Date dob) {this.name = name; this.age = age; this.dob = dob;}
+    }
 
 }
