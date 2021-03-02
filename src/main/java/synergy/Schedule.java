a723d49c9090548cab718e9b34e4ad4615c63af5
mhayescs19 <mhayescs19@gmail.com>
2021-03-02 08:31:22 -0800

    added full schedule display with test data

---
+++ src/main/java/synergy/Schedule.java
@@ -0,0 +1,18 @@
+package synergy;
+
+public class Schedule {
+
+    private Class period1;
+    private Class period2;
+    private Class period3;
+    private Class period4;
+    private Class period5;
+
+    public Schedule(Class period1, Class period2, Class period3, Class period4, Class period5) {
+        this.period1 = period1;
+        this.period2 = period2;
+        this.period3 = period3;
+        this.period4 = period4;
+        this.period5 = period5;
+    }
+}
